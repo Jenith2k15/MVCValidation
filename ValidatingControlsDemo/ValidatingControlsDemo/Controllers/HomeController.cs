f22dde1c018b93b147e54904343b93c362142959
Jenith2k15 <you@example.com>
2020-11-03 18:16:47 +0530

    MVCValidation

---
+++ ValidatingControlsDemo/ValidatingControlsDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ValidatingControlsDemo.Models;
+
+namespace ValidatingControlsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        Register register = new Register();
+        // GET: Home
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(register);
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(register);
+        }
+    }
+}
---
+++ ValidatingControlsDemo/ValidatingControlsDemo/Models/Register.cs
@@ -0,0 +1,60 @@
+﻿using Foolproof;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidatingControlsDemo.Models
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Email cannot be empty")]
+        [RegularExpression(@"[a-zA-Z0-9-]{1,}@([a-zA-Z\.])?[a-zA-Z]{1,}\.[a-zA-Z]{1,4}",
+            ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [System.ComponentModel.DataAnnotations.Compare("Password",
+            ErrorMessage = "Password and ConfirmPassword doesnt match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+
+        public SelectList Departments
+        {
+            get
+            {
+                return new SelectList(new List<Department>
+                {
+                    new Department{DeptId=1,DepartmentName="IT"},
+                    new Department{DeptId=2,DepartmentName="Payroll"},
+                    new Department{DeptId=3,DepartmentName="HR"},
+                    new Department{DeptId=4,DepartmentName="None"}
+                },"DeptId","DepartmentName");
+            }
+        }
+
+        [Required]
+        public string Gender { get; set; }
+
+        public List<Gender> Genders
+        {
+            get
+            {
+                return new List<Gender>
+                {
+                    new Gender{GenderId=1,GenderName="Male"},
+                    new Gender{GenderId=2,GenderName="Female"}
+                };
+            }
+        }
+    }
+}
